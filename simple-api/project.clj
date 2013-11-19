5289892bb30628c2096ed8423f0c5c38319a4049
Mike Ball <michaelhball@gmail.com>
2013-11-19 00:40:58 -0800

    Dependencies updates

--- simple-api/project.clj
+++ simple-api/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]]
+                 [cheshire "5.2.0"]
+                 [org.taoclj/tao "0.0.2-SNAPSHOT"]
+                 [org.taoclj/pure "0.0.2-SNAPSHOT"]]
 
   :ring {:handler simple-api.conf/dispatch
          :auto-refresh? false })
--- simple-site/project.clj
+++ simple-site/project.clj
@@ -5,7 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.reader "0.7.3"]
-                 [me.shenfeng/mustache "1.1"]]
+                 [me.shenfeng/mustache "1.1"]
+                 [org.taoclj/tao "0.0.2-SNAPSHOT"]
+                 [org.taoclj/pure "0.0.2-SNAPSHOT"]
+                 [org.taoclj/tao-mustache "0.0.2-SNAPSHOT"]]
 
   :ring {:handler simple-site.conf/dispatch
-         :auto-refresh? false })
+         :auto-reload? true
+         :auto-refresh? true })
