34a694bcc2f1526fb565ad2dc4afb55116e39f8c
Mike Ball <michaelhball@gmail.com>
2015-05-18 23:09:22 -0700

    Update dependencies

--- simple-api/project.clj
+++ simple-api/project.clj
@@ -1,11 +1,11 @@
 (defproject simple-api "0.1.0-SNAPSHOT"
-    
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.2.0"]
+
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [cheshire "5.4.0"]
                  [org.taoclj/tao "0.0.2-SNAPSHOT"]
                  [org.taoclj/pure "0.0.3-SNAPSHOT"]]
 
-  :plugins [[lein-ring "0.8.10"]]
+  :plugins [[lein-ring "0.9.4"]]
 
   :ring {:handler simple-api.conf/dispatch
          :auto-reload? true }
