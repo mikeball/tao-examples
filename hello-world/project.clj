0e48c365a18265d2c32d055bc1915a95cc1aeeb6
Mike Ball <michaelhball@gmail.com>
2015-05-18 23:05:39 -0700

    Update dependencies and cleanup

--- hello-world/project.clj
+++ hello-world/project.clj
@@ -1,17 +1,12 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
-  
-  :description "FIXME: write description"
-  
-  :url "http://example.com/FIXME"
-  
+  :description "Hello world example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.taoclj/tao "0.0.2-SNAPSHOT"]]
-  
 
-  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.taoclj/tao "0.0.2-SNAPSHOT"]]
+
+  :plugins [[lein-ring "0.9.4"]]
 
   :ring {:handler hello-world.app/dispatch
          :auto-refresh? false } )
